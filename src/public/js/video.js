9c4b3552865ba7e14177741c835706d5768df1bd
Yuki Ishihara <kai@exciter.jp>
2022-03-27 13:25:02 +0900

    動画登録時のバリデーション作成

--- src/public/js/video.js
+++ src/public/js/video.js
@@ -27,3 +27,76 @@ $('#thumb_upload').on('change', function(e) {
         reader.readAsDataURL(e.target.files[0]);
     }
 });
+
+// 動画のプレビューが開始されたら動画の先頭を切り取り、自動サムネイル作成準備
+const video_preview = document.querySelector('#video_preview');
+video_preview.addEventListener('playing', function () {
+    let canvas = document.getElementById('canvas');
+    let cCtx = canvas.getContext('2d');
+    let video = document.getElementById('video_preview');
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    // canvasに関数実行時の動画のフレームを描画
+    cCtx.drawImage(video, 0, 0);
+}, false);
+
+// 「登録」ボタン押下時のハンドラー
+$('#submit').on('click', function() {
+    let title_check = titleValidation();
+    let video_check = videoValidation();
+    console.log(title_check, video_check)
+    if (title_check && video_check) {
+        console.log('通過')
+        confirmModal();
+        removeValidation();
+    } else {
+        console.log('バリデーション失敗')
+    }
+});
+
+// 動画名のバリデーション
+const titleValidation = () => {
+    if (!$('#title').val()) {
+        $('#title').addClass('validation');
+        $('#title').attr('placeholder', '動画タイトルは必須です');
+        return false;
+    }
+    return true;
+}
+
+// 動画ファイルのバリデーション
+const videoValidation = () => {
+    if (!$('#video_upload').val()) {
+        $('#video_upload').addClass('validation');
+        return false;
+    }
+    return true;
+}
+
+// バリデーションを解除
+const removeValidation = () => {
+    $('#title').removeClass('validation');
+    $('#video_upload').removeClass('validation');
+}
+
+// 再入力でバリデーション解除
+$('#title').on('click', function() {
+    if (this.classList.contains('validation')) {
+        $(this).removeClass('validation');
+        $(this).attr('placeholder', '');
+    }
+});
+
+const confirmModal = () => {
+    // モーダルを表示
+    $('#confirm_modal').modal('show');
+    // 入力した値をモーダルに反映させる
+    $('#confirm_title').text($('#title').val());
+    $('#confirm_video').text($('#video_upload').prop('files')[0].name);
+    if ($('#thumb_upload').prop('files')[0]) {
+        const thumb = $('#thumb_upload').prop('files')[0].name;
+        $('#confirm_thumb').text(thumb);
+    } else {
+        $('#confirm_thumb').text('未選択');
+    }
+}
--- src/resources/views/video/create.blade.php
+++ src/resources/views/video/create.blade.php
@@ -1,6 +1,7 @@
 @extends('layout.header')
 @section('title', '動画登録画面')
 @section('content')
+<link rel="stylesheet" href="{{ asset('css/create.css') }}">
 
 @if ($errors->any())
     <div class="alert alert-danger">
@@ -15,16 +16,16 @@
 <div class="container mt-4">
     <div class="border p-4">
         <h1 class="h4 mb-4 font-weight-bold">動画登録画面</h1>
-        <form action="" id="form" name="video_form" enctype="multipart/form-data" method="POST">
+        {{-- <form action="" id="form" name="video_form" enctype="multipart/form-data" method="POST"> --}}
         @csrf
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">動画名</label>
-                <input type="text" name="" class="form-control" value="">
+                {{ Form::text('title', request('title'), ['class' => 'form-control', 'id' => 'title']) }}
             </div>
 
             <div class="mb-3">
                 <label for="exampleInputPassword1" class="form-label">動画ファイル</label>
-                <input type="file" name="" id="video_upload" class="form-control">
+                <input type="file" accept="video/mp4,video/qt" name="" id="video_upload" class="form-control">
             </div>
 
             {{-- 動画が選択されたらプレビューを表示 --}}
@@ -35,9 +36,9 @@
                     id="video_preview"
                     style="width:240px; height:180px;"
                 ></video>
-                {{-- サムネイル自動生成用 --}}
-                <canvas hidden type="hidden" id="canvas" class="canvas"></canvas>
             </div>
+            {{-- サムネイル自動生成用 --}}
+            <canvas hidden type="hidden" id="canvas" class="canvas"></canvas>
 
             <div class="mb-3">
                 <label for="exampleInputPassword1" class="form-label">動画サムネイル (未選択の場合、自動的に動画の先頭が設定されます)</label>
@@ -52,9 +53,41 @@
                     style="width:240px; height:180px;"
                 >
             </div>
-            <button type="submit" class="btn btn-primary">更新</button>
+            <button id="submit" class="btn btn-primary">登録</button>
 
-        </form>
+            {{-- 確認モーダル --}}
+            <div class="modal fade" id="confirm_modal" tabindex="-1">
+                <div class="modal-dialog modal-dialog-scrollable">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">入力内容の確認</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <div>
+                                <p class="text-muted">動画名</p>
+                                <p class="px-2" id="confirm_title"></p>
+                            </div>
+                            <div>
+                                <p class="text-muted">動画</p>
+                                <p class="px-2" id="confirm_video"></p>
+                            </div>
+                            <div>
+                                <p class="text-muted">サムネイル</p>
+                                <p class="px-2" id="confirm_thumb"></p>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">キャンセル</button>
+                            <button id="submit" class="btn btn-primary">保存</button>
+                            <div class="message"></div>
+                            <progress value="0" id="prog" max=100></progress>(<span id="pv" style="color:#00b200">0%</span>)
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        {{-- </form> --}}
     </div>
 </div>
 
