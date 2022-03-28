f9799b5d24bc27fd96efc93fdd5ba41201b837c6
Yuki Ishihara <kai@exciter.jp>
2022-03-28 12:13:14 +0900

    download機能追加 (内容はまだなし)

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,7 +1,9 @@
 $(function() {
 
-    let user_id;
+    // BOM
+    const BOM  = new Uint8Array([0xEF, 0xBB, 0xBF]);
 
+    let user_id;
     // 削除ボタンを押したらユーザーIDを取得
     $('#deleteModal').on('shown.bs.modal', function(e) {
         user_id = e.relatedTarget.id;
@@ -16,9 +18,58 @@ $(function() {
         form.submit();
     });
 
-});
 
-$('#output_file').on('click', function() {
-    console.log('download処理');
-});
+    const save = () => {
+        console.log('download処理');
+        workCsvData = [];
+        // ユーザー情報作成
+        createUserArray();
+        // ファイル保存
+        download();
+    };
 
+    const createUserArray = () => {
+        // for (let i = 0; i < 5; i++) {
+        //     workCsvData.push(i);
+        //     // console.log(i);
+        // }
+    };
+
+
+    // ファイル保存
+    const download = () => {
+        // CSVデータ整形
+        let csvString = createCsvString();
+        let blob = new Blob([BOM, csvString], {type: 'text/csv'});
+
+        // ダウンロード
+        const a = document.createElement('a');
+        a.download = createFileName();
+        a.href = (window.URL || window.webkitURL).createObjectURL(blob);;
+        a.click();
+    };
+
+    // ファイル名を生成 「ユーザー一覧_YYYY年MM月DD日」の形式で取得
+    const createFileName = () => {
+        const today = new Date();
+        const month = ('00' + (today.getMonth() + 1)).slice(-2);
+        const date  = ('00' + (today.getDate() + 1)).slice(-2);
+        return `ユーザー一覧_${today.getFullYear()}年${month}月${date}日`;
+    };
+
+    // CSV用配列から文字列に変換
+    const createCsvString = () => {
+        let csvString = workCsvData;
+        console.log(csvString);
+        return csvString;
+    };
+
+    $('#output_file').on('click', function() {
+        // console.log('download処理');
+        // if () { // バリデーション
+        save();
+        // }
+    });
+
+
+});
